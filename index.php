7ecdc2be918cd47c2ed6e850fea8ea4f82898b61
MARIO <sirisolasveo@gmail.com>
2019-03-01 09:51:07 -0500

    version 1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="../bootstrap/js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="../bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+   <h1>hola chiki baby</h1>
+</body>
+</html>
